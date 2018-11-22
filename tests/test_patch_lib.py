390f8d61a95dc352d9e64b5d2e35f6f8995f677e
n.kowalewska <n.kowalewska@clearcode.cc>
2018-11-22 15:31:28 +0100

    add raise messages to tests

--- tests/test_patch_lib.py
+++ tests/test_patch_lib.py
@@ -16,14 +16,16 @@ def test_remove():
 
 def test_remove_not_exist_element():
     delete_command = {"op": "remove", "path": "/fooo"}
-    with raises(PatchError):
+    with raises(PatchError) as error:
         patch_document({"test": "test"}, [delete_command])
+    assert error.value.message == "Can't remove not exist object."
 
 
 def test_add_existing_path():
     add_command = {"op": "add", "path": "/hello", "value": ["world"]}
-    with raises(PatchError):
+    with raises(PatchError) as error:
         patch_document({"hello": {}}, [add_command])
+    assert error.value.message == "Can't add existing path."
 
 
 def test_add():
@@ -49,8 +51,9 @@ def test_move_path_list():
 def test_move_not_exist_element():
     move_command = {"op": "move", "from": "/biscuits", "path": "/cookies"}
     old_document = {"biscuits": ["biscuit"], "cookies": ["cookie"]}
-    with raises(PatchError):
+    with raises(PatchError) as error:
         patch_document(old_document, [move_command])
+    assert error.value.message == "Can't move not exist object."
 
 
 def test_copy_value_to_list():
@@ -80,8 +83,9 @@ def test_copy_list_to_list():
 def test_copy_not_exist_element():
     copy_command = {"op": "move", "from": "/biscuits/1", "path": "/cookies"}
     old_document = {"biscuits": ["biscuit"], "cookies": {}}
-    with raises(PatchError):
+    with raises(PatchError) as error:
         patch_document(old_document, [copy_command])
+    assert error.value.message == "Can't copy not exist object."
 
 
 def test_test_when_value_exist():
@@ -97,6 +101,9 @@ def test_test_when_value_not_exist():
     test_command = {"op": "test",
                     "path": "/best_biscuit/name",
                     "value": "Choco Leibniz"}
-    with raises(PatchError):
+    with raises(PatchError) as error:
         patch_document(
-            {"best_biscuit": {'name': "Choko sticker"}}, [test_command])
+            {"best_biscuit": {'name': "Choko sticker"}
+             }, [test_command])
+    assert error.value.message == "Test fail." \
+                                  " value is different than expected."
