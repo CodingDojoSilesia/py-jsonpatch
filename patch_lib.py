f53340aabfb85e1f6953ab3d80e78c3427deedc8
Firemark <marpiechula@gmail.com>
2018-11-24 01:36:51 +0100

    add find_parent_and_key method

--- patch_lib.py
+++ patch_lib.py
@@ -6,3 +6,24 @@ def patch_document(document, commands):
     return document
     if "something_is_wrong":
         raise PatchError('something is wrong, dude')
+
+
+def find_parent_and_key(obj, path_list):
+    key, *path_list = path_list
+
+    if isinstance(obj, list):
+        key = int(key)
+
+    if not path_list:
+        return obj, key 
+
+    try:
+        nested_obj = obj[key]
+    except (KeyError, IndexError):
+        raise PatchError(f"{key!r} doesn't exist")
+
+    if not isinstance(nested_obj, dict) and not isinstance(nested_obj, list):
+        type_name = type(nested_obj).__name__
+        raise PatchError(f'{key!r} has a wrong nested type: {type_name}')
+
+    return find_parent_and_key(nested_obj, path_list)
--- tests/test_patch_lib.py
+++ tests/test_patch_lib.py
@@ -1,6 +1,68 @@
-from patch_lib import patch_document
+from patch_lib import patch_document, find_parent_and_key, PatchError
+
+import pytest
 
 
 def test_not_modified_document():
     document = patch_document({"test": "test"}, [])
     assert document == {"test": "test"}
+
+
+def test_find_parent_and_key_flat():
+    obj = {'key': 'foobar'}
+    parent, key = find_parent_and_key(obj,  ['key'])
+
+    assert parent is obj
+    assert key == 'key'
+
+
+def test_find_parent_and_key_nested_dict():
+    obj = {
+        'nested': {'key': 'foobar'}
+    }
+    parent, key = find_parent_and_key(obj,  ['nested', 'key'])
+
+    assert parent is obj['nested']
+    assert key == 'key'
+
+
+def test_find_parent_and_key_nested_dict_and_numeric_key():
+    obj = {
+        '123': {'key': 'foobar'}
+    }
+    parent, key = find_parent_and_key(obj,  ['123', 'key'])
+
+    assert parent is obj['123']
+    assert key == 'key'
+
+
+def test_find_parent_and_key_nested_list():
+    obj = {
+        'list': [
+            ['marek', 'foobar'],
+        ]
+    }
+    parent, key = find_parent_and_key(obj,  ['list', '0', '1'])
+
+    assert parent is obj['list'][0]
+    assert key == 1
+
+
+def test_find_parent_and_key_wrong_nested():
+    obj = {
+        'wrong_key': 'foobar'
+    }
+
+    with pytest.raises(PatchError) as exc:
+        parent, key = find_parent_and_key(obj,  ['wrong_key', 'value'])
+
+    assert exc.value.args[0] == "'wrong_key' has a wrong nested type: str"
+
+
+def test_find_parent_and_key_not_exist():
+    obj = {}
+
+    with pytest.raises(PatchError) as exc:
+        parent, key = find_parent_and_key(obj,  ['not_exist', 'foo'])
+
+    assert exc.value.args[0] == "'not_exist' doesn't exist"
