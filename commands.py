dcbd87ed9cdd2315e34f674128c155d75c182fa5
n.kowalewska <n.kowalewska@clearcode.cc>
2018-11-24 14:35:36 +0100

    move helpers to separate file

--- commands.py
+++ commands.py
@@ -1,59 +1,9 @@
-def get_path_elements(path_to_split):
-    splitted_path = path_to_split.split('/')
-    path_elements = filter(str.strip, splitted_path)
-    serialized_path = []
-    for element in path_elements:
-        try:
-            serialized_path.append(int(element))
-        except ValueError:
-            serialized_path.append(element)
-    return serialized_path
-
-
-def is_path_exist(document, *args):
-    try:
-        doc_reference = document.copy()
-        for i in args:
-            doc_reference = doc_reference[i]
-    except (IndexError, KeyError, TypeError) as e:
-        print(e)
-        return False
-    return True
-
-
-def remove_by_path(data, keys):
-    if len(keys) == 1:
-        value = data[keys[0]]
-        del data[keys[0]]
-        return value
-    return remove_by_path(data[keys[0]], keys[1:])
-
-
-def get_value_by_path(data, keys):
-    if len(keys) == 1:
-        return data[keys[0]]
-    return get_value_by_path(data[keys[0]], keys[1:])
-
-
-def add_value_by_path(data, keys, value_to_add):
-    if len(keys) == 1:
-        if type(data) == list:
-            data.insert(keys[0], value_to_add)
-        else:
-            data[keys[0]].append(value_to_add)
-        return True
-    add_value_by_path(data[keys[0]], keys[1:], value_to_add)
-
-
-def replace_value_by_path(data, keys, value_to_add):
-    if len(keys) == 1:
-        if type(data) == list:
-            data[keys[0]] = value_to_add
-        else:
-            data[keys[0]] = value_to_add
-        return True
-    replace_value_by_path(data[keys[0]], keys[1:], value_to_add)
-
+from helpers import get_path_elements
+from helpers import is_path_exist
+from helpers import add_value_by_path
+from helpers import replace_value_by_path
+from helpers import remove_by_path
+from helpers import get_value_by_path
 
 def remove(document, command):
     """
